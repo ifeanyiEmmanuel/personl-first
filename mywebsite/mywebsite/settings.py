d3fe0d6e8e2706fa993863da4d239cb8334bc97e
ifeanyi <ifeanyi835@gmail.com>
2023-02-20 17:43:44 +0100

    upload

--- mywebsite/mywebsite/settings.py
+++ mywebsite/mywebsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'nb7m1a2l))8cejip#*bwcr5pnkailc@ruvj@6eo(^dhu77$4+%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.43.1','127.0.0.1']
+ALLOWED_HOSTS = ['personl-first-production.up.railway.app','192.168.43.1','127.0.0.1']
 
 
 # Application definition
