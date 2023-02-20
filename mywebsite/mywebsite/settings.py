857a818e0daecbcc7073518f5048b9d1da256ed3
ifeanyi <ifeanyi835@gmail.com>
2023-02-20 17:48:17 +0100

    upload

--- mywebsite/mywebsite/settings.py
+++ mywebsite/mywebsite/settings.py
@@ -81,13 +81,25 @@ WSGI_APPLICATION = 'mywebsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'railway',
+        'USER':'postgres',
+        'PASSWORD':'f7BP0iMrTW6OKzZ1e1MX',
+        'HOST':'containers-us-west-147.railway.app',
+        'PORT':'5555'
 
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
